9bf7eff74858ac42e28484cc5e818a09ab1ec21d
KimJiWhan <jiwhan1015@yonsei.ac.kr>
2023-06-26 23:12:43 +0900

    23.06.26. Testbench Completed

--- Trainers/trainer.py
+++ Trainers/trainer.py
@@ -11,11 +11,12 @@ class trainer:
         self.lossF = nn.CrossEntropyLoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr, betas=(0.9, 0.999))
 
-    def step(self, image: torch.tensor, label: torch.tensor) -> float:
-        x = image.to(self.device)
-        y = label.to(self.device)
+    def step(self, image: torch.tensor, sound: torch.tensor, label: torch.tensor) -> float:
+        x1 = image.to(self.device)
+        x2 = sound.to(self.device)
+        y  = label.to(self.device)
         self.optimizer.zero_grad()
-        output = self.model.forward(x)
+        output = self.model.forward(x1, x2)
         loss = self.lossF(output, y)
         loss.backward()
         self.optimizer.step()
--- train.py
+++ train.py
@@ -20,8 +20,30 @@ elif torch.cuda.is_available():
 else:
     device = "cpu"
 
+category = [
+    "Cat",      # 0
+    "Chicken",  # 1
+    "Cow",      # 2
+    "Dog",      # 3
+    "Duck",     # 4
+    "Eagle",    # 5
+    "Lion",     # 6
+    "Pig",      # 7
+    "Sheep",    # 8
+    "Tiger"     # 9
+]
+
 batch_size = 16
 epoch      = 200
+
+
+def train(loader, n_epoch):
+    sum_loss = 0
+    model.train()
+    for image, sound, label in loader:
+        sum_loss = trainer.step(image, sound, label)
+        print("epoch: {}, loss: {}".format(n_epoch, sum_loss))
+
 
 
 if __name__ == "__main__":
@@ -30,4 +52,34 @@ if __name__ == "__main__":
 
     model   = M.CNN.to(device)
     trainer = T.trainer(0.0001, model, device)
+    train_load, test_load = D.getData()
+
+    model.load_state_dict(torch.load('model_params.pth'))
+
+    for i in range(1, epoch + 1):
+        train(train_load, i)
     
+    # Training Done
+    with torch.no_grad():
+        model.eval()
+        val = np.zeros(10, dtype=int)
+        correct = 0
+
+        for image, sound, label in test_load:
+            images = image.to(device)
+            sounds = sound.to(device)
+            labels = label.to(device)
+
+            output = model.forward(images, sounds)
+            result = torch.argmax(output, dim=1)
+            for res, ans in zip(result, labels):
+                if res == ans:
+                    val[res] += 1
+            correct += batch_size - torch.count_nonzero(result - labels)
+        
+        print("Final Result - Accuracy: {}\n\n".format(100 * correct / 50))
+        for i in range(10):
+            print("class: {}: {} / 5".format(category[i], val[i]))
+        
+        torch.save(model.state_dict(), 'model_params.pth')
+
